ff34f9db303071e69db225db29aa11965edecd68
Rani Saad <rsaad@live.ca>
2021-04-30 21:35:45 -0400

    Added all prompts

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateTeamProfile = require("./src/teamTemplate.js");
+// const { choices } = require("yargs");
 
 const promptUser = () => {
   return inquirer.prompt([
     {
       type: "input",
-      name: "project",
-      message: "What is the name of your project? (Required)",
+      name: "manager",
+      message: "What is the manager's name? (Required)",
       validate: nameInput => {
         if (nameInput) {
           return true;
@@ -18,71 +19,119 @@ const promptUser = () => {
     },
     {
       type: "input",
-      name: "description",
-      message: "Please enter a description of your project",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Please provide installation instructions",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Please provide instructions on how to use this generator",
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message: "Please provide contribution guidelines",
-    },
-    {
-      type: "input",
-      name: "test",
-      message: "Please provide instructions on how to test this generator",
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "Choose one of the following licenses",
-      choices: [
-        "Boost",
-        "MIT",
-        "BSD",
-        "Apache 2.0",
-        "GNU General Public License V3.0",
-      ],
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Please provide your GitHub Username (Required)",
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          return false;
-        }
-      },
+      name: "employeeID",
+      message: "Please enter the manager's employee ID",
     },
     {
       type: "input",
       name: "email",
-      message: "Please provide your email",
+      message: "Please provide the manager's email address",
+    },
+    {
+      type: "input",
+      name: "office",
+      message: "Please enter the manager's office number",
+    },
+    {
+      type: "list",
+      name: "addMember",
+      message: "Would you like to add an Engineer or Intern to your team?",
+      choices: ["Engineer", "Intern", "Finish team building"],
+    },
+  ]);
+};
+//
+
+const engineerPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "engineerName",
+      message: "Please enter the Engineer's name",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "engineerID",
+      message: "Please enter the Engineer's ID",
+    },
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "Please enter the Engineer's email",
+    },
+    {
+      type: "input",
+      name: "engineerGitHub",
+      message: "Please enter the Engineer's Github Username",
+    },
+    {
+      type: "list",
+      name: "addMember",
+      message: "Would you like to add another Engineer or Intern to your team?",
+      choices: ["Engineer", "Intern", "Finish team building"],
     },
   ]);
 };
 
-const writeFile = readmePage => {
-  return fs.writeFileSync("Readme.md", readmePage);
+const internPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "internName",
+      message: "Please enter the Intern's name",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "internID",
+      message: "Please enter the Intern's ID",
+    },
+    {
+      type: "input",
+      name: "internEmail",
+      message: "Please enter the Intern's school",
+    },
+    {
+      type: "list",
+      name: "addMember",
+      message: "Would you like to add another Intern or Engineer to your team?",
+      choices: ["Engineer", "Intern", "Finish team building"],
+    },
+  ]);
+};
+
+const writeFile = teamPage => {
+  return fs.writeFileSync("", teamPage);
 };
 
 promptUser()
-  .then(readmeData => {
-    return generateReadme(readmeData);
+  .then(teamData => {
+    console.log(teamData);
+    return engineerPrompt();
+
+    //return generateTeamProfile(teamData);
   })
-  .then(readmePage => {
-    return writeFile(readmePage);
+  .then(teamData => {
+    console.log(teamData);
+    console.log(inquirer.choices);
+    return internPrompt();
+  })
+  .then(teamPage => {
+    console.log(teamPage);
+    //return writeFile(teamPage);
   })
   .catch(err => {
     console.log(err);
